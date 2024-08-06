c8456c04e5926c67481e5f744be2b12d12601ba0
Pavel Balashov <pavel.balashov1@gmail.com>
2024-08-06 11:12:16 +0200

    chore: improve debugging in app-nest-1

--- apps/app-nest-1/webpack.config.js
+++ apps/app-nest-1/webpack.config.js
@@ -4,6 +4,15 @@ const { join } = require('path');
 module.exports = {
   output: {
     path: join(__dirname, '../../dist/apps/app-nest-1'),
+    /**
+     * Enable breakpoints in the source code when debugging using
+     * VSCode JavaScript Debug Terminal and CWD is the root of the Nx workspace
+     *
+     * https://github.com/nrwl/nx/issues/21517#issuecomment-2242902642
+     * https://webpack.js.org/configuration/output/#outputdevtoolmodulefilenametemplate
+     * https://webpack.js.org/configuration/output/#outputdevtoolfallbackmodulefilenametemplate
+     */
+    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
   },
   plugins: [
     // https://nx.dev/recipes/webpack/webpack-plugins#nxappwebpackplugin
@@ -16,6 +25,7 @@ module.exports = {
       optimization: false,
       outputHashing: 'none',
       skipTypeChecking: true, // Default: false
+      sourceMap: true, // Default: false
     }),
   ],
 };

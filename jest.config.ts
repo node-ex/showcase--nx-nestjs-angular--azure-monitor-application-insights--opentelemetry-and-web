c9661147cfee14e61f19a62194a0b9af17099c9f
Pavel Balashov <pavel.balashov1@gmail.com>
2024-08-06 10:07:28 +0200

    docs: add documentation comments to the root Jest configs

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,6 @@
 import { getJestProjectsAsync } from '@nx/jest';
 
 export default async () => ({
+  // https://github.com/nrwl/nx/blob/master/packages/jest/src/utils/config/get-jest-projects.ts
   projects: await getJestProjectsAsync(),
 });

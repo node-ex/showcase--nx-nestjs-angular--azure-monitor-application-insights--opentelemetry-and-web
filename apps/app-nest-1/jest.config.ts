67fb2a964551fe5e428b7d2440f522db0a87dcd5
Pavel Balashov <pavel.balashov1@gmail.com>
2024-08-06 10:08:28 +0200

    chore: disable type checking when running unit tests in app-nest-1

--- apps/app-nest-1/jest.config.ts
+++ apps/app-nest-1/jest.config.ts
@@ -4,7 +4,20 @@ export default {
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        /*
+         * Disable type-checking to speed up test runs
+         *
+         * https://kulshekhar.github.io/ts-jest/docs/getting-started/options/
+         * https://github.com/kulshekhar/ts-jest/blob/main/website/docs/getting-started/options/diagnostics.md
+         * https://github.com/kulshekhar/ts-jest/issues/822#issuecomment-1961616203
+         */
+        diagnostics: false,
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/app-nest-1',

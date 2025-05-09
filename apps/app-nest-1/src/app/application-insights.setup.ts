6a5df46317f3575957df43fb3a1ceff4be5f83d6
Pavel Balashov <pavel.balashov1@gmail.com>
2025-05-09 23:25:18 +0200

    chore: rename App Insights env var

--- apps/app-nest-1/src/app/application-insights.setup.ts
+++ apps/app-nest-1/src/app/application-insights.setup.ts
@@ -6,11 +6,11 @@
 import { ApplicationInsightsUtils } from './application-insights.utils';
 
 // console.log(
-//   "process.env['AZURE_MONITOR_APPLICATION_INSIGHTS_CONNECTION_STRING']",
-//   process.env['AZURE_MONITOR_APPLICATION_INSIGHTS_CONNECTION_STRING'],
+//   "process.env['PUBLIC_AZURE_MONITOR_APPLICATION_INSIGHTS_CONNECTION_STRING']",
+//   process.env['PUBLIC_AZURE_MONITOR_APPLICATION_INSIGHTS_CONNECTION_STRING'],
 // );
 
 ApplicationInsightsUtils.initialize(
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-  process.env['AZURE_MONITOR_APPLICATION_INSIGHTS_CONNECTION_STRING']!,
+  process.env['PUBLIC_AZURE_MONITOR_APPLICATION_INSIGHTS_CONNECTION_STRING']!,
 );
